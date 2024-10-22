30e0efd04f27db6eaedf0517591cb0ebe9059086
mika1551 <dashoksit@gmail.com>
2024-10-22 22:30:33 +0300

    Документация в файл circle.py  была добавлена

--- circle.py
+++ circle.py
@@ -3,8 +3,8 @@ import math
 
 def area(r):
     return math.pi * r * r
-
+''' Принимает переменную r, и возвращает площадь круга'''
 
 def perimeter(r):
     return 2 * math.pi * r
-
+''' Принимает переменную r, и возвращает периметр круга'''
