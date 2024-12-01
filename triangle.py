cf48c70c3ff360b4a92d3ac0b0c5c64bc34feabc
Daria Sitnikova <467479@niuitmo.ru>
2024-12-01 20:20:20 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,9 +1,12 @@
 import math
 
 def area(a, b, c):
+    if a + b <= c or a + c <= b or b + c <= a:
+        raise ValueError("Invalid triangle sides")
     s = (a + b + c) / 2
-    return math.sqrt(s * (s-a) *  (s-b) * (s-c))
-
+    return math.sqrt(s * (s - a) * (s - b) * (s - c))
 
 def perimeter(a, b, c):
+    if a + b <= c or a + c <= b or b + c <= a:
+        raise ValueError("Invalid triangle sides")
     return a + b + c
