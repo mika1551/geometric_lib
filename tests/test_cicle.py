6ac0b33bd6eda98a4452aedb4fe61f5d7e777eee
Daria Sitnikova <467479@niuitmo.ru>
2024-12-06 18:45:40 +0300

    Update test_cicle.py

--- tests/test_cicle.py
+++ tests/test_cicle.py
@@ -1,35 +1,19 @@
 import unittest
+from shapes.circle import area, perimeter
 from math import pi
-from circle import area, perimeter
-
 
 class TestCircleFunctions(unittest.TestCase):
     def test_area(self):
-        """Проверка вычисления площади круга для различных радиусов."""
-        test_cases = [
-            (1, pi),
-            (2, 4 * pi),
-            (0, 0),
-            (5.5, pi * 5.5**2),
-            (0.1, pi * 0.1**2),
-        ]
-        for radius, expected in test_cases:
-            with self.subTest(radius=radius):
-                self.assertAlmostEqual(area(radius), expected)
-
+        self.assertAlmostEqual(area(1), pi)
+        self.assertAlmostEqual(area(2), 4 * pi)
+        self.assertAlmostEqual(area(0), 0)
+        self.assertAlmostEqual(area(5.5), pi * 5.5 ** 2)
+    
     def test_perimeter(self):
-        """Проверка вычисления периметра круга для различных радиусов."""
-        test_cases = [
-            (1, 2 * pi),
-            (2, 4 * pi),
-            (0, 0),
-            (5.5, 2 * pi * 5.5),
-            (0.1, 2 * pi * 0.1),
-        ]
-        for radius, expected in test_cases:
-            with self.subTest(radius=radius):
-                self.assertAlmostEqual(perimeter(radius), expected)
-
+        self.assertAlmostEqual(perimeter(1), 2 * pi)
+        self.assertAlmostEqual(perimeter(2), 4 * pi)
+        self.assertAlmostEqual(perimeter(0), 0)
+        self.assertAlmostEqual(perimeter(5.5), 2 * pi * 5.5)
 
 if __name__ == "__main__":
     unittest.main()
