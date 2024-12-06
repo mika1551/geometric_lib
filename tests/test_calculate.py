73161d0e6ecf0c37a01c0442147b4c7ed5beed2a
Daria Sitnikova <467479@niuitmo.ru>
2024-12-06 20:26:31 +0300

    Update test_calculate.py

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,32 +1,34 @@
 import unittest
 from calculate import calc
 
-
 class TestCalculate(unittest.TestCase):
-    def test_addition(self):
-        self.assertEqual(calc('+', 2, 3), 5)
+    def test_calc_circle(self):
+        self.assertAlmostEqual(calc('circle', 'area', [1]), 3.141592653589793, places=10)
+        self.assertAlmostEqual(calc('circle', 'perimeter', [1]), 6.283185307179586, places=10)
 
-    def test_subtraction(self):
-        self.assertEqual(calc('-', 5, 2), 3)
+    def test_calc_square(self):
+        self.assertEqual(calc('square', 'area', [2]), 4)
+        self.assertEqual(calc('square', 'perimeter', [2]), 8)
 
-    def test_multiplication(self):
-        self.assertEqual(calc('*', 3, 4), 12)
+    def test_calc_triangle(self):
+        self.assertAlmostEqual(calc('triangle', 'area', [3, 4, 5]), 6.0, places=10)
+        self.assertEqual(calc('triangle', 'perimeter', [3, 4, 5]), 12)
 
-    def test_division(self):
-        self.assertEqual(calc('/', 8, 2), 4)
+    def test_invalid_figure(self):
+        with self.assertRaises(ValueError):
+            calc('pentagon', 'area', [1])
 
     def test_invalid_function(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(AssertionError):
             calc('circle', 'volume', [1])
 
     def test_invalid_size_length(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(AssertionError):
             calc('circle', 'area', [1, 2])
 
     def test_invalid_triangle_sides(self):
         with self.assertRaises(ValueError):
             calc('triangle', 'area', [1, 2, 10])
-
 
 if __name__ == '__main__':
     unittest.main()
