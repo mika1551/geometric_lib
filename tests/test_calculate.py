9591fbbd320502ae45331f9ac7a4ded58ec16cfd
Daria Sitnikova <467479@niuitmo.ru>
2024-12-06 19:27:20 +0300

    Update test_calculate.py

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,30 +1,14 @@
 import unittest
 from calculate import calc
-class TestCalculateFunctions(unittest.TestCase):
-    def test_valid_calculations(self):
-        self.assertEqual(calc('circle', 'area', [1]), 3.141592653589793)
-        self.assertEqual(calc('square', 'area', [2]), 4)
-        self.assertEqual(calc('triangle', 'perimeter', [3, 4, 5]), 12)
-    
-    def test_invalid_figure(self):
-        with self.assertRaises(ValueError):
-            calc('pentagon', 'area', [1])
-    
-    def test_invalid_function(self):
-        with self.assertRaises(ValueError):
-            calc('circle', 'volume', [1])
-    
-    def test_invalid_size_length(self):
-        with self.assertRaises(ValueError):
-            calc('circle', 'area', [1, 2])
-    
-    def test_negative_sizes(self):
-        with self.assertRaises(ValueError):
-            calc('square', 'area', [-1])
-    
-    def test_invalid_triangle_sides(self):
-        with self.assertRaises(ValueError):
-            calc('triangle', 'area', [1, 2, 10])
 
-if __name__ == "__main__":
+class TestCalculate(unittest.TestCase):
+    def test_calc(self):
+        self.assertEqual(calc('+', 2, 3), 5)
+        self.assertEqual(calc('-', 2, 3), -1)
+        self.assertEqual(calc('*', 2, 3), 6)
+        self.assertAlmostEqual(calc('/', 3, 2), 1.5, places=10)
+        with self.assertRaises(ValueError):
+            calc('/', 3, 0)
+
+if __name__ == '__main__':
     unittest.main()
