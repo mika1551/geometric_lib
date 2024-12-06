18f0f76781bb5cb719c975d02b6ef0a359e6b768
Daria Sitnikova <467479@niuitmo.ru>
2024-12-06 20:45:25 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,19 +1,18 @@
 import math
 
+
 def area(a, b, c):
-    if a < 0 or b < 0 or c < 0:
-        raise ValueError("Side lengths cannot be negative.")
+    if a <= 0 or b <= 0 or c <= 0:
+        raise ValueError("Side lengths must be positive.")
     if a + b <= c or a + c <= b or b + c <= a:
-        raise ValueError("Invalid triangle sides")
-    
-    # Геронова формула для площади треугольника
-    p = (a + b + c) / 2
-    return math.sqrt(p * (p - a) * (p - b) * (p - c))
+        raise ValueError("Invalid triangle sides.")
+    s = (a + b + c) / 2
+    return math.sqrt(s * (s - a) * (s - b) * (s - c))
+
 
 def perimeter(a, b, c):
-    if a < 0 or b < 0 or c < 0:
-        raise ValueError("Side lengths cannot be negative.")
+    if a <= 0 or b <= 0 or c <= 0:
+        raise ValueError("Side lengths must be positive.")
     if a + b <= c or a + c <= b or b + c <= a:
-        raise ValueError("Invalid triangle sides")
-    
+        raise ValueError("Invalid triangle sides.")
     return a + b + c
