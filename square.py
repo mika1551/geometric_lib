3a0bc03e7c07a365c4bcdaac326a15b040a6917f
Daria Sitnikova <467479@niuitmo.ru>
2024-12-06 18:34:01 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,11 +1,11 @@
 def area(a):
     if (a < 0):
-        raise ValueError("Can't use negative numbers")
+        raise ValueError("Can't use '-' numbers")
 
     return a * a
 
 def perimeter(a):
     if (a < 0):
         # added verification of borders cuz them cant be below zeros
-        raise ValueError("Can't use negative numbers")
+        raise ValueError("Can't use '-' numbers")
     return 4 * a
