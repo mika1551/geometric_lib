c20ea58d4e41e82cf31237ebcc20d2198f960215
Daria Sitnikova <467479@niuitmo.ru>
2024-12-06 18:33:18 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,10 +1,12 @@
-import math
+def area(a):
+    if (a < 0):
+        raise ValueError("Can't use '-' numbers")
+
+    return a * a
 
 
-def area(r):
-    return math.pi * r * r
-
-
-def perimeter(r):
-    return 2 * math.pi * r
-
+def perimeter(a):
+    if (a < 0):
+        # added verification of borders cuz them cant be below zeros
+        raise ValueError("Can't use '-' numbers")
+    return 4 * a
