bf3e4878693285cd9ae7295785d128047a983e1d
Daria Sitnikova <467479@niuitmo.ru>
2024-12-06 20:41:39 +0300

    Update test_cicle.py

--- tests/test_cicle.py
+++ tests/test_cicle.py
@@ -1,6 +1,7 @@
 import unittest
 from circle import area, perimeter
 
+
 class TestCircleFunctions(unittest.TestCase):
     def test_area(self):
         self.assertAlmostEqual(area(1), 3.141592653589793, places=10)
@@ -9,6 +10,7 @@ class TestCircleFunctions(unittest.TestCase):
     def test_perimeter(self):
         self.assertAlmostEqual(perimeter(1), 6.283185307179586, places=10)
         self.assertAlmostEqual(perimeter(0), 0, places=10)
+
 
 if __name__ == '__main__':
     unittest.main()
