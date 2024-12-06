058b50e988b3f505d5204cf7c9ca04a4c0fd92bb
Daria Sitnikova <467479@niuitmo.ru>
2024-12-06 19:25:18 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,16 +1,9 @@
-import unittest
-from square import area, perimeter
+def area(side):
+    if side < 0:
+        raise ValueError("Side length cannot be negative.")
+    return side ** 2
 
-class TestSquareFunctions(unittest.TestCase):
-    def test_area(self):
-        self.assertAlmostEqual(area(0.1), 0.01, places=10)
-        self.assertAlmostEqual(area(5), 25, places=10)
-        self.assertAlmostEqual(area(0), 0, places=10)
-
-    def test_perimeter(self):
-        self.assertAlmostEqual(perimeter(0.1), 0.4, places=10)
-        self.assertAlmostEqual(perimeter(5), 20, places=10)
-        self.assertAlmostEqual(perimeter(0), 0, places=10)
-
-if __name__ == '__main__':
-    unittest.main()
+def perimeter(side):
+    if side < 0:
+        raise ValueError("Side length cannot be negative.")
+    return 4 * side
