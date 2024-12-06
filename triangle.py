34654d2cd86895908202021ff5ac142dc256c762
Daria Sitnikova <467479@niuitmo.ru>
2024-12-06 18:20:13 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,12 +1,15 @@
-import math
-
 def area(a, b, c):
-    if a + b <= c or a + c <= b or b + c <= a:
+    if (a + b > abs(c) and a + c > abs(b) and b + c > abs(a)):
+        p = (a + b + c) / 2
+        # before return semi-perimeter changed on geron formula;
+        return (p * (p - a) * (p - b) * (p - c))/2
+    else:
+        # added verification of existing triangle
         raise ValueError("Invalid triangle sides")
-    s = (a + b + c) / 2
-    return math.sqrt(s * (s - a) * (s - b) * (s - c))
+
 
 def perimeter(a, b, c):
-    if a + b <= c or a + c <= b or b + c <= a:
+    if (a + b > abs(c) and a + c > abs(b) and b + c > abs(a)):
+        return a + b + c
+    else:
         raise ValueError("Invalid triangle sides")
-    return a + b + c
