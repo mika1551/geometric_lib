37dcfc31baca6fcd81fb0bb13b61025981ca0509
Daria Sitnikova <467479@niuitmo.ru>
2024-12-06 20:42:42 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,12 +1,14 @@
 import unittest
 from triangle import area, perimeter
 
+
 class TestTriangleFunctions(unittest.TestCase):
     def test_area(self):
         self.assertAlmostEqual(area(3, 4, 5), 6.0, places=10)
 
     def test_perimeter(self):
         self.assertEqual(perimeter(3, 4, 5), 12)
+
 
 if __name__ == '__main__':
     unittest.main()
